aa7e0e5d10a5d936ef3aa5cb22d75bc7f4942488
WackyCodes <rezonefood@gmail.com>
2019-09-29 20:03:33 +0530

    GoWacky One

---
+++ app/src/main/java/wackycodes/gowacky/Go_Activity11.java
@@ -0,0 +1,13 @@
+package wackycodes.gowacky;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Go_Activity11 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_go_11);
+    }
+}
